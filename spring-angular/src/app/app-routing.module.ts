3804c468e0d6abc421165456f4f079ec0d21a5d3
Onke-M <onmweza@gmail.com>
2023-03-23 15:17:35 +0200

    Added grid view for displaying books

--- spring-angular/src/app/app-routing.module.ts
+++ spring-angular/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BookCatalogComponent } from './book-catalog/book-catalog.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{path: 'book-catalog', component: BookCatalogComponent},
+{path: '', component: BookCatalogComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- spring-angular/src/app/app.module.ts
+++ spring-angular/src/app/app.module.ts
@@ -4,10 +4,16 @@ import { MaterialModule } from './material.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BookCatalogComponent } from './book-catalog/book-catalog.component';
+import { CartPageComponent } from './cart-page/cart-page.component';
+import { LoginPageComponent } from './login-page/login-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BookCatalogComponent,
+    CartPageComponent,
+    LoginPageComponent
   ],
   imports: [
     BrowserModule,
