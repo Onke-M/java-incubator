b7bbfd255e0d3ac78f85e9b9b1e1986a64133f6f
Onke-M <onmweza@gmail.com>
2023-03-28 16:18:27 +0200

    Updated payment and details page UI

--- spring-angular/src/app/payment-checkout-page/payment-checkout-page.component.ts
+++ spring-angular/src/app/payment-checkout-page/payment-checkout-page.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-payment-checkout-page',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./payment-checkout-page.component.css']
 })
 export class PaymentCheckoutPageComponent {
+  paymentDetails!: FormGroup;
 
+  async ngOnInit(){
+
+    this.paymentDetails = new FormGroup({
+      cardNumber: new FormControl('', [Validators.required]),
+      expiryDate: new FormControl('', [Validators.required]),
+      cvvNumber: new FormControl('', [Validators.required])
+    })
+  }
 }
