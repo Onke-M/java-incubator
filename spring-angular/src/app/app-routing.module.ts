51e27daa780054fb303edb0ef930aa2ec9eef426
Onke-M <onmweza@gmail.com>
2023-03-29 13:54:08 +0200

    Added login page

--- spring-angular/src/app/app-routing.module.ts
+++ spring-angular/src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BookCatalogComponent } from './book-catalog/book-catalog.component';
 import { CheckoutPageComponent } from './checkout-page/checkout-page.component';
+import { LoginPageComponent } from './login-page/login-page.component';
 
 const routes: Routes = [
 {path: 'book-catalog', component: BookCatalogComponent},
 {path: '', component: BookCatalogComponent},
-{path: 'checkout', component: CheckoutPageComponent}
+{path: 'checkout', component: CheckoutPageComponent},
+{path: 'login', component: LoginPageComponent},
 ];
 
 @NgModule({
--- spring-angular/src/app/login-page/login-page.component.ts
+++ spring-angular/src/app/login-page/login-page.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, ElementRef, HostListener, OnInit, ViewChild } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login-page',
@@ -6,5 +9,45 @@ import { Component } from '@angular/core';
   styleUrls: ['./login-page.component.css']
 })
 export class LoginPageComponent {
+  loginForm!: FormGroup;
+  invalidLogin!: boolean;
+  users: any = [];
+  public showPass: boolean = false;
 
+  @ViewChild('password')
+  public password!: ElementRef;
+
+  constructor(
+    public fb: FormBuilder,
+    public router: Router,
+    private http: HttpClient
+  ) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required]],
+    });
+  }
+
+  async loginUser(loginForm: any) {
+    const credentials = {
+      email: loginForm.value.email,
+      password: loginForm.value.password
+    }
+
+    console.log(credentials);
+  }
+
+  showHide() {
+    let x = this.password.nativeElement;
+    if (x.type === "password") {
+      x.setAttribute('type', "text")
+      this.showPass = false
+    } else {
+      x.setAttribute('type', "password")
+      this.showPass = true
+    }
+  }
 }
+
+
+
