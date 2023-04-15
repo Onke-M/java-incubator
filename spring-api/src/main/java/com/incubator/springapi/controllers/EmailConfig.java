957cb9031bddd927ac16e0105b7230cc6295a0c5
Onke-M <onmweza@gmail.com>
2023-04-15 16:03:21 +0200

    removed

--- spring-api/src/main/java/com/incubator/springapi/controllers/EmailConfig.java
+++ spring-api/src/main/java/com/incubator/springapi/controllers/EmailConfig.java
@@ -15,8 +15,8 @@ public class EmailConfig {
         mailSender.setHost("smtp.gmail.com");
         mailSender.setPort(587);
 
-        mailSender.setUsername("onkemweza1@gmail.com");
-        mailSender.setPassword("kecqqgpsxxraucwv");
+        mailSender.setUsername("user");
+        mailSender.setPassword("user");
 
         Properties props = mailSender.getJavaMailProperties();
         props.put("mail.transport.protocol", "smtp");
